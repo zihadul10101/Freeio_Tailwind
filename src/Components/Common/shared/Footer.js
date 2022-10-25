24303d3e3d8069bd6f4723dd3711f541bb1ca078
zihadul islam <zihadul10101@gmail.com>
2022-10-25 21:04:59 +0600

    footer html contant added

--- src/Components/Common/shared/Footer.js
+++ src/Components/Common/shared/Footer.js
@@ -2,9 +2,70 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-                       <h1>Footer hear</h1> 
-
+        <div className="container">
+            <div className="top">
+                <div className="privacy-policy">
+                    <p>Terms of Service  </p>
+                    <p> Privacy Policy  </p>
+                    <p>  Site Map  </p>
+                </div>
+                <div className="follow">
+                    <div>
+                        <p>Follow Us</p>
+                    </div>
+                    <div>
+                        <img src='' />
+                        <img src='' />
+                        <img src='' />
+                        <img src='' />
+                    </div>
+                </div>
+            </div>
+            <div className="center">
+                <div className="">
+                    <p>About</p>
+                    <img src='' />
+                </div>
+                <div className="">
+                    <p>Categories</p>
+                    <img src='' />
+                </div>
+                <div className="">
+                    <p>Support</p>
+                    <img src='' />
+                </div>
+                <div>
+                    <p>Subscribe</p>
+                    <div className="btn">
+                        <p>Your email address</p>
+                        <h1>Send</h1>
+                    </div>
+                </div>
+                <div className="">
+                    <h1>Apps</h1>
+                    <div className="">
+                        <img src='' />
+                        <p>iOS App
+                            Android App</p>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom">
+                <p>© Freeio. 2022 CreativeLayers. All rights reserved.</p>
+                <div className="languse-usd">
+                    <div>
+                        <p>US$ USD</p>
+                        <img src='' />
+                    </div>
+                    <div>
+                        <p>English</p>
+                        <img src='' />
+                    </div>
+                </div>
+            </div>
+            <div className="back-to-top">
+                <img src='' />
+            </div>
         </div>
     );
 };
