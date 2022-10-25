b99919f3c35e35b96d85e8d3dfdfad1298f07f08
zihadul islam <zihadul10101@gmail.com>
2022-10-25 16:10:03 +0600

    hero contant added

--- src/Pages/Home/Hero.js
+++ src/Pages/Home/Hero.js
@@ -3,11 +3,19 @@ import '../../Assets/Styles/css/Hero.css'
 const Hero = () => {
     return (
         <div className="container">
-        <div className='hero-container'>
-<p className='hero-title'>Hire the best freelancers for any job, online.</p>
-<p className='hero-title-small'>Millions of people use freeio.com to turn their 
-ideas into reality.</p>
-        </div>
+            <div className='hero-container'>
+                <p className='hero-title'>Hire the best freelancers for any job, online.</p>
+                <p className='hero-title-small'>Millions of people use freeio.com to turn their<br />
+                    ideas into reality.</p>
+                     <div className='search'>
+                     <div className="search-input-btn">
+                        
+                        </div>
+                        <div className="search-btn">
+    
+                        </div>
+                        </div>    
+            </div>
         </div>
     );
 };
