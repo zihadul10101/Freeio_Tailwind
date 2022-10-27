4e37504c3392821df6c02aa30c9078a33da70751
zihadul islam <zihadul10101@gmail.com>
2022-10-27 12:54:50 +0600

    PopularServices style added

--- src/Pages/Home/PopularServices.js
+++ src/Pages/Home/PopularServices.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../../Assets/Styles/css/PopularServices.css';
 import popular from '../../Assets/Image/hero/Surface 1.png'
+import start from '../../Assets/Image/hero/ster (1).png';
+import { popularServices } from '../../../src/utiltes/LocalDatabase';
+
 const PopularServices = () => {
     return (
         <div className='container'>
@@ -19,36 +22,41 @@ const PopularServices = () => {
                 </div>
             </div>
             <div className="popular-contant">
-                <div className="popular-contant-box">
-                    <div className="popular-contant-box-img">
-                        <img src={popular} alt="popular-contant-box-img" />
-                    </div>
-                    <div className="popular-head-review-bottom">
+                {
+                    popularServices.map(service => <>
+                        <div className="popular-contant-box">
+                            <div className="popular-contant-box-img">
+                                <img src={popular} alt="popular-contant-box-img" />
+                            </div>
+                            <div className="popular-head-review-bottom">
 
 
-                        <div className="head">
-                            <p>Web & App Design</p>
-                            <h2>I will design modern websites in figma or adobe xd</h2>
-                        </div>
-                        <div className="review">
-                            <div>
-                                <img src="" />
+                                <div className="head">
+                                    <p>{service.item}</p>
+                                    <h3 className="hover">{service.title}</h3>
+                                    
+                                </div>
+                                <div className="review">
+                                    <div>
+                                        <img src={start} />
+                                    </div>
+                                    <div>
+                                        <p><span>4.82 94</span> reviews</p>
+                                    </div>
+                                </div>
+                                <div className="bottom">
+                                    <div className="bottom-freelancer-name">
+                                        <div className='img'>  <img src={popular} /></div>
+                                        <div> <p>{service.name}</p> </div>
+                                    </div>
+                                    <div className="bottom-freelancer-price">
+                                        <p>Starting at <span>$983</span></p>
+                                    </div>
+                                </div>
                             </div>
-                            <div>
-                                <p>4.82 94 reviews</p>
-                            </div>
                         </div>
-                        <div className="bottom">
-                          <div className="bottom-freelancer-name">
-                          <div>  <img src={popular} /></div>
-                          <div> <p>Wanda Runo</p> </div>
-                          </div>
-                          <div className="bottom-freelancer-price">
-                            <p>Starting at $983</p>
-                          </div>
-                        </div>
-                    </div>
-                </div>
+                    </>)
+                }
             </div>
 
         </div>
--- src/utiltes/LocalDatabase.js
+++ src/utiltes/LocalDatabase.js
@@ -1,31 +1,106 @@
 import service from '../Assets/Image/top-selling-service/Vector.svg'
+
 export const topSellingServices = [
     {
         id: 1,
-        img:service,
+        img: service,
         title: 'Post a job',
-        description:'It’s free and easy to post a job. Simply fill in a title, description.'
+        description: 'It’s free and easy to post a job. Simply fill in a title, description.'
 
     },
     {
         id: 2,
-        img:service
+        img: service
         ,
         title: 'Post a job',
-        description:'It’s free and easy to post a job. Simply fill in a title, description.'
+        description: 'It’s free and easy to post a job. Simply fill in a title, description.'
     },
     {
         id: 3,
-        img:service
+        img: service
         ,
         title: 'Post a job',
-        description:'It’s free and easy to post a job. Simply fill in a title, description.'
+        description: 'It’s free and easy to post a job. Simply fill in a title, description.'
     },
     {
         id: 4,
-        img:service
+        img: service
         ,
         title: 'Post a job',
-        description:'It’s free and easy to post a job. Simply fill in a title, description.'
+        description: 'It’s free and easy to post a job. Simply fill in a title, description.'
     }
-]
+];
+export const popularServices = [
+    {
+        id: 1,
+        img: service,
+        title: 'I will design modern websites in figma or adobe xd',
+        item: 'Web & App Design'
+        ,
+        name: 'Wanda Runo'
+
+    },
+    {
+        id: 2,
+        img: service
+        ,
+        title: 'I will design modern websites in figma or adobe xd',
+        item: 'Web & App Design'
+        ,
+        name: 'Wanda Runo'
+    },
+    {
+        id: 3,
+        img: service
+        ,
+        title: 'I will design modern websites in figma or adobe xd',
+        item: 'Web & App Design'
+        ,
+        name: 'Wanda Runo'
+    },
+    {
+        id: 4,
+        img: service
+        ,
+        title: 'I will design modern websites in figma or adobe xd',
+        item: 'Web & App Design'
+        ,
+        name: 'Wanda Runo'
+    },
+    {
+        id: 5,
+        img: service
+        ,
+        title: 'I will design modern websites in figma or adobe xd',
+        item: 'Web & App Design'
+        ,
+        name: 'Wanda Runo'
+    },
+    {
+        id: 6,
+        img: service
+        ,
+        title: 'I will design modern websites in figma or adobe xd',
+        item: 'Web & App Design'
+        ,
+        name: 'Wanda Runo'
+    },
+    {
+        id: 7,
+        img: service
+        ,
+        title: 'I will design modern websites in figma or adobe xd',
+        item: 'Web & App Design'
+        ,
+        name: 'Wanda Runo'
+    },
+    {
+        id: 8,
+        img: service
+        ,
+        title: 'I will design modern websites in figma or adobe xd',
+        item: 'Web & App Design'
+        ,
+        name: 'Wanda Runo'
+    },
+];
