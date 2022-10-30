5414729e5ca70c72121a7b36fc2cad4f1b5e1ec1
zihadul islam <zihadul10101@gmail.com>
2022-10-30 21:22:29 +0600

    nevbar section is html added now

--- src/Components/Common/shared/Navbar.js
+++ src/Components/Common/shared/Navbar.js
@@ -5,7 +5,59 @@ import nav_icon from '../../../Assets/Image/hero/nav_icon.svg';
 const Navbar = () => {
     return (
         <div className="container" >
-            <h1>Header</h1>
+            <div className="navbar">
+                <div className="navbar-left">
+                    <ul>
+                        <li className="logo">
+                            <img src={logo} alt="" />
+                            <h4>Freeio.</h4>
+                        </li>
+                        <li className="category-logo">
+                            <img src={logo} alt="" />
+                            <h4>Category</h4>
+                        </li>
+                        <li>
+                            <h5> Home </h5>
+                            <img src={logo} alt="" />
+                        </li>
+                        <li>
+                            <h5> Browse Jobs</h5>
+                            <img src={logo} alt="" />
+                        </li>
+                        <li>
+                            <h5> Services</h5>
+                            <img src={logo} alt="" />
+                        </li>
+                        <li>
+                            <h5> Blog</h5>
+                            <img src={logo} alt="" />
+                        </li>
+                        <li>
+                            <h5>Pages</h5>
+                            <img src={logo} alt="" />
+                        </li>
+                        <li>
+                            <h5> Contact</h5>
+                            <img src={logo} alt="" />
+                        </li>
+                    </ul>
+                </div>
+                <div className="navbar-right">
+                    <ul>
+                        <li> <img src={logo} alt="" /></li>
+                        <li>
+                            <h5>Become a Seller</h5>
+                        </li>
+                        <li>
+                            <h5>Sign In</h5>
+                        </li>
+                        <li>
+                            <button>Join</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
         </div>
     );
 };
